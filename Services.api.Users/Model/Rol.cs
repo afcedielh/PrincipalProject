809c3a9428af4966f1642d7155dfd70eb17356e7
afced <afced@ANDRESCEDIEL>
2022-07-19 17:24:26 -0500

    Agregar archivos de proyecto.

---
+++ Services.api.Users/Model/Rol.cs
@@ -0,0 +1,9 @@
+﻿namespace Services.api.Users.Model
+{
+    public class Rol
+    {
+        public int RolId { get; set; }
+        public string? RolName { get; set; }
+        public int Permissions { get; set; }
+    }
+}
---
+++ Services.api.Users/Model/User.cs
@@ -0,0 +1,11 @@
+﻿namespace Services.api.Users.Model
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string? Email { get; set; }
+        public string? Pass { get; set; }
+        public string? UserName { get; set; }
+        public string? Name { get; set; }
+    }
+}
